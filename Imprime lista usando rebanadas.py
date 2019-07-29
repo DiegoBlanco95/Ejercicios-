d786743b4cf2061258cec91f9520e86647293479
DiegoBlanco95 <51379632+DiegoBlanco95@users.noreply.github.com>
2019-07-29 14:19:25 -0300

    Add files via upload

---
+++ Imprime lista usando rebanadas.py
@@ -0,0 +1,18 @@
+"""Crear una lista con los cuadrados de los números entre 1 y N (ambos incluidos),
+donde N se ingresa desde el teclado. Luego se solicita imprimir los últimos 10 valores
+de la lista, utilizando la técnica de las rebanandas."""
+
+import random
+def carga_elementos(valor,lista):
+    numero = 1
+    while numero < valor:
+        lista.append(numero**2)
+        numero += 1
+    return lista
+
+
+valor = int(input("Ingrese un valor: "))
+lista = []
+lista = carga_elementos(valor, lista)
+print(lista)
+print(lista[len(lista) - 10:])
